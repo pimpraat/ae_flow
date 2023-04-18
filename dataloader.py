dc477c4c04a643aa708e1942325c02f86bbbbe52
JanAthmer <janathmer@hotmail.com>
2023-04-18 13:19:34 +0200

    removed random comments

--- dataloader.py
+++ dataloader.py
@@ -1,19 +1,3 @@
-################################################################################
-# MIT License
-#
-# Copyright (c) 2022
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to conditions.
-#
-# Author: Deep Learning Course | Autumn 2022
-# Date Created: 2022-11-25
-################################################################################
-
 import torchvision
 import torchvision.transforms as T
 import torch
@@ -29,7 +13,7 @@ def loads(data_dir,ext,label):
     convert_tensor = T.ToTensor()
     resizer = T.Resize((256,256))
     image_list = []
-    for filename in glob.glob(data_dir+'*.'+ext): #assuming gif
+    for filename in glob.glob(data_dir+'*.'+ext):
         im=Image.open(filename)
         im = resizer(im)
         im = convert_tensor(im)
@@ -38,22 +22,7 @@ def loads(data_dir,ext,label):
 
 
 def load(data_dir,batch_size=128, num_workers=4):
-    """
-    Returns data loaders for 4-bit MNIST dataset, i.e. values between 0 and 15.
 
-    Inputs:
-        root - Directory in which the MNIST dataset should be downloaded. It is better to
-               use the same directory as the part2 of the assignment to prevent duplicate
-               downloads.
-        batch_size - Batch size to use for the data loaders
-        num_workers - Number of workers to use in the data loaders.
-        download - If True, MNIST is downloaded if it cannot be found in the specified
-                   root directory.
-    """
-
-    # Each data loader returns tuples of (img, label)
-    # For the generative models we don't need the labels, which we need to take into account
-    # when writing the train code.
     train_dir = data_dir+"train/NORMAL/"
     val_dir_0 = data_dir+"val/NORMAL/"
     val_dir_1 = data_dir+"val/PNEUMONIA/"
