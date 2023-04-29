10a11c62cb56799c06f0374c0807256ec878e477
Jan Athmer <lcur1624@test3.irc.sara.nl.irc.sara.nl>
2023-04-29 16:42:42 +0200

    still not working, but getting closer

--- model/flow.py
+++ model/flow.py
@@ -6,22 +6,19 @@ import FrEIA.framework as Ff
 import FrEIA.modules as Fm
 
 class FlowModule(nn.Module):
-    """
-        Inputs:     
-    """
 
-    
-    def __init__(self,subnet_conv_3x3_1x1):
-        super(FlowModule, self).__init__()
-    
-        self.inn = Ff.SequenceINN(16*16)
-        for k in range(8):
-            self.inn.append(Fm.AllInOneBlock, subnet_constructor=subnet_conv_3x3_1x1, permute_soft=True)
-
+    def __init__(self):
+            super(FlowModule, self).__init__()
         
-    # def subnet_conv(c_in, c_out):
-    #     return nn.Sequential(nn.Conv2d(c_in, 256,   3, padding=1), nn.ReLU(),
-    #                         nn.Conv2d(256,  c_out, 3, padding=1))
+            self.inn = Ff.SequenceINN(16*16)
+            for k in range(8):
+                self.inn.append(Fm.AllInOneBlock, subnet_constructor=FlowModule.subnet_conv_3x3_1x1, permute_soft=True)
+
+
+    def subnet_conv_3x3_1x1(c_in, c_out):
+        return nn.Sequential(nn.Conv2d(c_in, 256,   3, padding=1), nn.ReLU(),
+                            nn.Conv2d(256,  c_out, 1))
+        
 
     def forward(self, x):
         z, log_jac_det = self.inn(x)
