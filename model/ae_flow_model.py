835af9a98250c43dc35e689db5c116584f2d9d00
Pim Praat <pimpraat@gmail.com>
2023-05-04 13:39:10 +0200

    performance tuning, w&b, sample images

--- model/ae_flow_model.py
+++ model/ae_flow_model.py
@@ -2,7 +2,8 @@ import torch.nn as nn
 import torch
 from nflows.distributions import normal
 import torchmetrics
-
+from torchvision.utils import make_grid, save_image
+import torchvision
 
 from model.decoder import Decoder
 from model.encoder import Encoder
@@ -44,11 +45,21 @@ class AE_Flow_Model(nn.Module):
     def get_anomaly_score(self, _beta, original_x, reconstructed_x):
         Sflow = - self.get_flow_loss(return_logz=True)
         Srecon = - torchmetrics.functional.structural_similarity_index_measure(reduction=None, preds=reconstructed_x, target=original_x)
+
+        print(f"Sflow: {Sflow}, Srecon:{Srecon}")
         return _beta * Sflow + (1-_beta)*Srecon
     
+    #TODO: Finish implementation of this function
     # A function used to (easily) sample the same set of images. Using 
     def sample_images(self):
-        pass
-
-        # now save the images to file
-
+        device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+        grid = make_grid([self.sample_images, self.model(self.sample_images.to(device)).squeeze(dim=1)], nrow = 2)
+        images = torchvision.transforms.ToPILImage()(grid)
+        return images
+    
+    @property
+    def device(self):
+        """
+        Property function to get the device on which the generator is
+        """
+        return next(self.parameters()).device
--- model/utils.py
+++ model/utils.py
@@ -16,9 +16,9 @@ def calculate_given_threshold(proposed_threshold, anomaly_scores, true_labels):
     #     pass
 
     score = f1_score(y_true=true_labels[0].cpu(), y_pred=preds[0].cpu())
-    print(f"Calculate threshold using threshold-value of: {proposed_threshold}, found F1 score to be: {score}")
-    print(f"With {true_labels[0].cpu()} as true labels, and {preds[0].cpu()} using the predictions/threshold")
-    print(f"Anomaly scores were: {anomaly_scores}")
+    # print(f"Calculate threshold using threshold-value of: {proposed_threshold}, found F1 score to be: {score}")
+    # print(f"With {true_labels[0].cpu()} as true labels, and {preds[0].cpu()} using the predictions/threshold")
+    # print(f"Anomaly scores were: {anomaly_scores}")
     return score
 
 def optimize_threshold(anomaly_scores, true_labels):
@@ -26,5 +26,5 @@ def optimize_threshold(anomaly_scores, true_labels):
     # print(f"Anomaly scores: {anomaly_scores}, True labels: {true_labels}")
     opt_threshold = scipy.optimize.bisect(f=calculate_given_threshold, a=0.000, b=1.000, 
                                           args=(anomaly_scores, true_labels), xtol=0.01)
-    print(f"Found optimal threshold: {opt_threshold}")
+    # print(f"Found optimal threshold: {opt_threshold}")
     return opt_threshold
