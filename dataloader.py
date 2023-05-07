b80b152e4f72a2e09716c816601ca845da11dabd
janathmer <janathmer@hotmail.com>
2023-05-07 16:22:36 +0200

    more data

--- dataloader.py
+++ dataloader.py
@@ -34,15 +34,51 @@ def loads(data_dir,ext,label):
 
 def load(data_dir,batch_size=64, num_workers=4):
 
-    train_dir = data_dir+"train/NORMAL/"
-    val_dir_0 = data_dir+"val/NORMAL/"
-    val_dir_1 = data_dir+"val/PNEUMONIA/"
-    test_dir_0 = data_dir+"test/NORMAL/"
-    test_dir_1 = data_dir+"test/PNEUMONIA/"
+    if data_dir == "chest_xray":
+        train_dir = "data/"+data_dir+"/train/NORMAL/"
 
-    train_dataset = loads(train_dir,"jpeg",0)
-    val_dataset = loads(val_dir_0,"jpeg",0)+loads(val_dir_1,"jpeg",1)
-    test_set = loads(test_dir_0,"jpeg",0)+loads(test_dir_1,"jpeg",1)
+        val_dir_0 = "data/"+data_dir+"/val/NORMAL/"
+        val_dir_1 = "data/"+data_dir+"/val/PNEUMONIA/"
+
+        test_dir_0 = "data/"+data_dir+"/test/NORMAL/"
+        test_dir_1 = "data/"+data_dir+"/test/PNEUMONIA/"
+        
+        train_dataset = loads(train_dir,"jpeg",0)
+        val_dataset = loads(val_dir_0,"jpeg",0)+loads(val_dir_1,"jpeg",1)
+
+        #is different per dataset
+        test_loader_abnormal = data.DataLoader(
+            loads(test_dir_1,"jpeg",1), batch_size=batch_size, shuffle=False, num_workers=num_workers,
+            drop_last=False, pin_memory=False)        
+
+    elif data_dir == "OCT2017":
+        train_dir = "data/"+data_dir+"/train/NORMAL/"
+
+        val_dir_0 = "data/"+data_dir+"/val/NORMAL/"
+        val_dir_1 = "data/"+data_dir+"/val/CNV/"
+        val_dir_3 = "data/"+data_dir+"/val/DME/"
+        val_dir_2 = "data/"+data_dir+"/val/DRUSEN/"
+
+        test_dir_0 = "data/"+data_dir+"/test/NORMAL/"
+        test_dir_1 = "data/"+data_dir+"/test/CNV/"
+        test_dir_2 = "data/"+data_dir+"/test/DME/"
+        test_dir_3 = "data/"+data_dir+"/test/DRUSEN/"
+
+        train_dataset = loads(train_dir,"jpeg",0)
+        
+        val_dataset = loads(val_dir_0,"jpeg",0)
+        +loads(val_dir_1,"jpeg",1)
+        +loads(val_dir_2,"jpeg",1)
+        +loads(val_dir_3,"jpeg",1)
+
+        test_abnormal = loads(test_dir_1,"jpeg",1)
+        +loads(test_dir_2,"jpeg",1)
+        +loads(test_dir_3,"jpeg",1)
+
+        test_loader_abnormal = data.DataLoader(
+            test_abnormal, batch_size=batch_size, shuffle=False, num_workers=num_workers,
+            drop_last=False, pin_memory=False)
+
 
 
     ## As we use Nvidia GPU's pin_memory for speedup using pinned memmory
@@ -54,9 +90,7 @@ def load(data_dir,batch_size=64, num_workers=4):
     test_loader_normal = data.DataLoader(
         loads(test_dir_0,"jpeg",0), batch_size=batch_size, shuffle=False, num_workers=num_workers,
         drop_last=False, pin_memory=False)
-    test_loader_abnormal = data.DataLoader(
-        loads(test_dir_1,"jpeg",1), batch_size=batch_size, shuffle=False, num_workers=num_workers,
-        drop_last=False, pin_memory=False)
+
 
     return train_loader, val_loader, [test_loader_normal, test_loader_abnormal]
 
