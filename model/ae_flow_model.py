fixes with grads + sign in a loss

model/ae_flow_model.py
@@ -70,7 +70,8 @@ class AE_Flow_Model(nn.Module):
         """        
         log_z = self.get_flow_loss(return_logz=True)
         Sflow = -log_z
-        Srecon = torchmetrics.functional.structural_similarity_index_measure(reduction=None, preds=reconstructed_x, target=original_x)
+        assert(Sflow > 0)
+        Srecon = -torchmetrics.functional.structural_similarity_index_measure(reduction=None, preds=reconstructed_x, target=original_x)
         return _beta * Sflow + (1-_beta)*Srecon
     
     def sample_images(self):